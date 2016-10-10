c58336b61c199792bda81ce191c1465b67178d02
andelf <andelf@gmail.com>
2016-10-10 17:36:22 +0800

    rust-mmgraph: rm usage of rawlink

--- rust-mmgraph/lib.rs
+++ rust-mmgraph/lib.rs
@@ -1,7 +1,7 @@
 #![feature(box_syntax)]
 
 use std::ops::{Deref, DerefMut};
-use std::marker::PhantomData;
+use std::fmt;
 
 mod rawlink;
 use self::rawlink::Rawlink;
@@ -13,83 +13,89 @@ pub enum Direction {
     Both,
 }
 
-#[derive(Debug)]
-pub struct Graph {
-    nodes: Vec<Option<Box<Node>>>,
-    rels: Vec<Option<Box<Relationship>>>,
-    reuse_nodes: Vec<usize>,
-    reuse_rels: Vec<usize>,
-}
 
 #[derive(Debug)]
-pub struct Node {
+pub struct Node<N> {
     id: usize,
-    name: i32,
+    name: N,
     next_start_rel: Option<usize>,
     next_end_rel: Option<usize>, /* TODO: support prop
                                   * prop: BTreeMap<String, Property>,
                                   * TODO: label */
 }
 
-pub struct NodeMut<'a> {
-    node: Rawlink<Node>,
-    graph: Rawlink<Graph>,
-    _marker: PhantomData<&'a u8>
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct NodeIx(usize);
+
+pub struct NodeMut<'a, N: 'a, E: 'a> {
+    node: NodeIx,
+    graph: &'a mut Graph<N, E>,
 }
 
-impl<'a> NodeMut<'a> {
-    pub fn create_rel_to(&mut self, other: &Node) -> RelationshipMut {
-        let n = unsafe { self.node.resolve().expect("can't reuse after delete.") };
-        if let Some(g) = unsafe { self.graph.resolve_mut() } {
-            let id = if let Some(reuse_id) = g.reuse_rels.pop() {
-                reuse_id
-            } else {
-                g.rels.push(None);
-                g.rels.len() - 1
-            };
+impl<'a, N, E> Deref for NodeMut<'a, N, E> {
+    type Target = Node<N>;
+    fn deref(&self) -> &Self::Target {
+        &**self.graph.nodes[self.node.0].as_ref().unwrap()
+    }
+}
 
-            let mut rel = Relationship {
-                id: id,
-                start_node: n.id,
-                start_prev: None,
-                start_next: None,
-                end_node: other.id,
-                end_prev: None,
-                end_next: None,
-            };
-            // handle relationship links
-            if let Some(next_rid) = n.next_start_rel {
-                rel.start_next = Some(next_rid);
-                g.rels[next_rid].as_mut().map(|r| r.start_prev = Some(id));
-            }
-            // FIXME: unsafe change n.next_rel
-            g.nodes[n.id].as_mut().map(|n| n.next_start_rel = Some(id));
+impl<'a, N, E> DerefMut for NodeMut<'a, N, E> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut **self.graph.nodes[self.node.0].as_mut().unwrap()
+    }
+}
 
-            if let Some(next_rid) = other.next_end_rel {
-                rel.end_next = Some(next_rid);
-                g.rels[next_rid].as_mut().map(|r| r.end_prev = Some(id));
-            }
-            g.nodes[other.id].as_mut().map(|n| n.next_end_rel = Some(id));
-
-            g.rels[id] = Some(box rel);
-            RelationshipMut {
-                rel: Rawlink::some(g.rels[id].as_mut().unwrap()),
-                graph: self.graph,
-            }
+impl<'a, N, E> NodeMut<'a, N, E> {
+    pub fn create_rel_to(&mut self, other: NodeIx, weight: E) -> RelationshipIx {
+        let &Node { id: my_id, next_start_rel: my_next_start_rel_id, .. } =
+            &**self.graph.nodes[self.node.0].as_ref().unwrap();
+        let id = if let Some(reuse_id) = self.graph.reuse_rels.pop() {
+            reuse_id
         } else {
-            panic!("mem error!")
+            self.graph.rels.push(None);
+            self.graph.rels.len() - 1
+        };
+
+        let &Node { id: other_id, next_end_rel: other_next_end_rel_id, .. } =
+            &**self.graph.nodes[other.0].as_ref().unwrap();
+
+        let mut rel = Relationship {
+            id: id,
+            weight: weight,
+            start_node: my_id,
+            start_prev: None,
+            start_next: None,
+            end_node: other_id,
+            end_prev: None,
+            end_next: None,
+        };
+        // handle relationship links
+        if let Some(next_rid) = my_next_start_rel_id {
+            rel.start_next = Some(next_rid);
+            self.graph.rels[next_rid].as_mut().map(|r| r.start_prev = Some(id));
         }
+        // FIXME: unsafe change n.next_rel
+        self.graph.nodes[my_id].as_mut().map(|n| n.next_start_rel = Some(id));
+
+        if let Some(next_rid) = other_next_end_rel_id {
+            rel.end_next = Some(next_rid);
+            self.graph.rels[next_rid].as_mut().map(|r| r.end_prev = Some(id));
+        }
+        self.graph.nodes[other_id].as_mut().map(|n| n.next_end_rel = Some(id));
+
+        self.graph.rels[id] = Some(box rel);
+        RelationshipIx(id)
     }
 
     pub fn delete(&mut self) {
         unimplemented!()
     }
 
-    pub fn iter_rels(&self) -> ::std::slice::Iter<Relationship> {
+    pub fn iter_rels(&self) -> ::std::slice::Iter<Relationship<E>> {
         unimplemented!()
     }
 
-    pub fn iter_rels_of_dir(&self, dir: Direction) -> ::std::slice::Iter<Relationship> {
+    pub fn iter_rels_of_dir(&self, dir: Direction) -> ::std::slice::Iter<Relationship<E>> {
         unimplemented!()
     }
 
@@ -98,8 +104,7 @@ impl<'a> NodeMut<'a> {
     }
 
     pub fn get_degree_of_dir(&self, dir: Direction) -> usize {
-        let n = unsafe { self.node.resolve().expect("can't reuse after delete.") };
-        let g = unsafe { self.graph.resolve().expect("mem error") };
+        let n = self.graph.nodes[self.node.0].as_ref().unwrap();
 
         let mut degree = 0;
 
@@ -107,14 +112,14 @@ impl<'a> NodeMut<'a> {
             Direction::Outgoing => {
                 let mut next = n.next_start_rel;
                 while let Some(mut rid) = next {
-                    next = g.rels[rid].as_ref().unwrap().start_next;
+                    next = self.graph.rels[rid].as_ref().unwrap().start_next;
                     degree += 1;
                 }
             }
             Direction::Incoming => {
                 let mut next = n.next_end_rel;
                 while let Some(mut rid) = next {
-                    next = g.rels[rid].as_ref().unwrap().end_next;
+                    next = self.graph.rels[rid].as_ref().unwrap().end_next;
                     degree += 1;
                 }
             }
@@ -127,23 +132,11 @@ impl<'a> NodeMut<'a> {
     }
 }
 
-impl<'a> Deref for NodeMut<'a> {
-    type Target = Node;
-    fn deref(&self) -> &Node {
-        unsafe { self.node.resolve().expect("Can't reuse a deleted node.") }
-    }
-}
-
-impl<'a> DerefMut for NodeMut<'a> {
-    fn deref_mut(&mut self) -> &mut Node {
-        unsafe { self.node.resolve_mut().expect("Can't reuse a deleted node.") }
-    }
-}
-
 
 #[derive(Debug)]
-pub struct Relationship {
+pub struct Relationship<E> {
     id: usize, // idx in Graph
+    weight: E,
     start_node: usize,
     start_prev: Option<usize>,
     start_next: Option<usize>,
@@ -153,32 +146,62 @@ pub struct Relationship {
                               * prop: BTreeMap<String, Property>, */
 }
 
-pub struct RelationshipMut {
-    rel: Rawlink<Relationship>,
-    graph: Rawlink<Graph>,
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+pub struct RelationshipIx(usize);
+
+pub struct RelationshipMut<'a, N: 'a, E: 'a> {
+    rel: RelationshipIx,
+    graph: &'a mut Graph<N, E>,
 }
 
-impl RelationshipMut {
-    pub fn start_node(&self) -> &Node {
-        unimplemented!()
-    }
-
-    pub fn end_node(&self) -> &Node {
-        unimplemented!()
-    }
-
-    pub fn other_node(&self, node: &Node) -> &Node {
-        unimplemented!()
-    }
-
-    pub fn nodes(&self) -> ::std::slice::Iter<Node> {
-        unimplemented!()
+impl<'a, N, E> Deref for RelationshipMut<'a, N, E> {
+    type Target = Relationship<E>;
+    fn deref(&self) -> &Self::Target {
+        &**self.graph.rels[self.rel.0].as_ref().unwrap()
     }
 }
 
+impl<'a, N, E> DerefMut for RelationshipMut<'a, N, E> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut **self.graph.rels[self.rel.0].as_mut().unwrap()
+    }
+}
 
-impl Graph {
-    pub fn new() -> Graph {
+impl<'a, N, E> RelationshipMut<'a, N, E> {
+    pub fn start_node(&self) -> NodeIx {
+        NodeIx(self.graph.rels[self.rel.0].as_ref().unwrap().start_node)
+    }
+
+    pub fn end_node(&self) -> NodeIx {
+        NodeIx(self.graph.rels[self.rel.0].as_ref().unwrap().end_node)
+    }
+
+    pub fn other_node(&self, node: NodeIx) -> NodeIx {
+        if self.start_node() == node {
+            self.end_node()
+        } else if self.end_node() == node {
+            self.start_node()
+        } else {
+            panic!("node is not belong to this rel")
+        }
+    }
+
+    pub fn nodes(&self) -> ::std::slice::Iter<Node<N>> {
+        unimplemented!()
+    }
+}
+
+
+#[derive(Debug)]
+pub struct Graph<N, E> {
+    nodes: Vec<Option<Box<Node<N>>>>,
+    rels: Vec<Option<Box<Relationship<E>>>>,
+    reuse_nodes: Vec<usize>,
+    reuse_rels: Vec<usize>,
+}
+
+impl<N: fmt::Debug + PartialEq, E> Graph<N, E> {
+    pub fn new() -> Graph<N, E> {
         Graph {
             nodes: vec![],
             rels: vec![],
@@ -187,7 +210,7 @@ impl Graph {
         }
     }
 
-    pub fn with_capacity(n: usize) -> Graph {
+    pub fn with_capacity(n: usize) -> Graph<N, E> {
         Graph {
             nodes: Vec::with_capacity(n),
             rels: Vec::with_capacity(n),
@@ -196,11 +219,20 @@ impl Graph {
         }
     }
 
-    pub fn create_node<'a, 'b: 'a>(&'a mut self) -> NodeMut<'b> {
-        self.create_node_with_name(2333)
+    pub fn find_or_create_node(&mut self, name: N) -> NodeIx {
+        let nix = self.nodes
+                      .iter()
+                      .find(|n| n.as_ref().map_or(false, |n| n.name == name))
+                      .and_then(|n| n.as_ref())
+                      .map(|n| NodeIx(n.id));
+        if let Some(nix) = nix {
+            nix
+        } else {
+            self.create_node(name)
+        }
     }
 
-    pub fn create_node_with_name<'a, 'b: 'a>(&'a mut self, name: i32) -> NodeMut<'b> {
+    pub fn create_node(&mut self, name: N) -> NodeIx {
         let id = if let Some(reuse_id) = self.reuse_nodes.pop() {
             reuse_id
         } else {
@@ -215,56 +247,93 @@ impl Graph {
         };
         self.nodes[id] = Some(box node);
 
+        NodeIx(id)
+    }
+
+    pub fn node(&self, id: NodeIx) -> Option<&Node<N>> {
+        self.nodes.get(id.0).and_then(|n| n.as_ref().map(|n| &**n))
+    }
+
+    pub fn node_mut(&mut self, id: NodeIx) -> Option<NodeMut<N, E>> {
+        Some(NodeMut {
+            node: id,
+            graph: self,
+        })
+    }
+
+    pub fn with_new_node(&mut self, name: N) -> NodeMut<N, E> {
+        let id = self.create_node(name);
         NodeMut {
-            node: Rawlink::some(self.nodes[id].as_mut().unwrap()),
-            graph: Rawlink::some(self),
-            _marker: PhantomData
+            node: id,
+            graph: self,
         }
     }
 
-    pub fn get_node_by_id(&self, id: usize) -> Option<&Node> {
-        self.nodes.get(id).and_then(|n| n.as_ref().map(|n| &**n))
+    pub fn with_found_or_new_node(&mut self, name: N) -> NodeMut<N, E> {
+        let node = self.nodes
+                       .iter()
+                       .find(|n| n.as_ref().map_or(false, |n| n.name == name))
+                       .and_then(|n| n.as_ref())
+                       .map(|n| NodeIx(n.id));
+        if let Some(id) = node {
+            NodeMut {
+                node: id,
+                graph: self,
+            }
+        } else {
+            self.with_new_node(name)
+        }
     }
 
-    pub fn get_rel_by_id(&self, id: usize) -> Option<&Relationship> {
-        self.rels.get(id).and_then(|r| r.as_ref().map(|r| &**r))
+    pub fn rel(&self, id: RelationshipIx) -> Option<&Relationship<E>> {
+        self.rels.get(id.0).and_then(|r| r.as_ref().map(|r| &**r))
     }
 
-    pub fn iter_all_nodes(&self) -> Vec<&Node> {
+    pub fn rel_mut(&mut self, id: RelationshipIx) -> Option<RelationshipMut<N, E>> {
+        Some(RelationshipMut {
+            rel: id,
+            graph: self,
+        })
+    }
+
+    pub fn iter_all_nodes(&self) -> Vec<&Node<N>> {
         self.nodes
             .iter()
             .filter(|n| n.is_some())
             .map(|n| n.as_ref().map(|n| &**n).unwrap())
-            .collect::<Vec<&Node>>()
+            .collect::<Vec<&Node<_>>>()
     }
 
-    pub fn iter_all_rels(&self) -> Vec<&Relationship> {
+    pub fn iter_all_rels(&self) -> Vec<&Relationship<E>> {
         self.rels
             .iter()
             .filter(|r| r.is_some())
             .map(|r| r.as_ref().map(|r| &**r).unwrap())
-            .collect::<Vec<&Relationship>>()
+            .collect::<Vec<&Relationship<_>>>()
     }
 
-    pub fn dump(&self) {
-        println!("");
-        self.nodes
-            .iter()
-            .filter(|n| n.is_some())
-            .map(|n| {
-                println!("{:?}", n.as_ref().unwrap());
-            })
-            .last();
-
-        self.rels
-            .iter()
-            .filter(|r| r.is_some())
-            .map(|r| {
-                println!("{:?}", r.as_ref().unwrap());
-            })
-            .last();
-    }
-
+    //    pub fn dump(&self) {
+    // println!("");
+    // self.nodes
+    // .iter()
+    // .filter(|n| n.is_some())
+    // .map(|n| {
+    // println!("{:?}", n.as_ref().unwrap());
+    // })
+    // .last();
+    //
+    // self.rels
+    // .iter()
+    // .filter(|r| r.is_some())
+    // .map(|r| {
+    // println!("{:?}", r.as_ref().unwrap());
+    // })
+    // .last();
+    // }
+    //
+    //
+}
+impl<N: fmt::Debug + fmt::Display, E: fmt::Debug + fmt::Display> Graph<N, E> {
     pub fn to_dot(&self) -> String {
         let mut dot = String::new();
 
@@ -283,7 +352,7 @@ impl Graph {
                 dot.push_str(&format!("    {} -> {} [label = \"{}\"];\n",
                                       r.start_node,
                                       r.end_node,
-                                      r.debug()));
+                                      r.weight));
             })
             .last();
         dot.push_str("}\n");
@@ -292,9 +361,9 @@ impl Graph {
 }
 
 
-impl Node {
-    pub fn get_id(&self) -> usize {
-        self.id
+impl<N> Node<N> {
+    pub fn get_id(&self) -> NodeIx {
+        NodeIx(self.id)
     }
 
     pub fn has_rel(&self) -> bool {
@@ -303,9 +372,9 @@ impl Node {
 }
 
 
-impl Relationship {
-    pub fn get_id(&self) -> usize {
-        self.id
+impl<E> Relationship<E> {
+    pub fn get_id(&self) -> RelationshipIx {
+        RelationshipIx(self.id)
     }
 
     pub fn debug(&self) -> String {
@@ -323,12 +392,12 @@ impl Relationship {
 
 #[test]
 fn test_graph_create_node() {
-    let mut g = Graph::new();
-    let mut v = g.create_node();
-    let mut x = g.create_node_with_name(666);
-    let mut u = g.create_node_with_name(999);
-    let w = g.create_node_with_name(999);
-    let q = g.create_node_with_name(555);
+    let mut g = Graph::<i32, i32>::new();
+    let mut v = g.create_node(233);
+    let mut x = g.create_node(666);
+    let mut u = g.create_node(999);
+    let w = g.create_node(999);
+    let q = g.create_node(555);
 
     assert!(!q.has_rel());
 
@@ -344,11 +413,4 @@ fn test_graph_create_node() {
 
     println!("\n {}", g.to_dot());
 
-}
-
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {}
 }
--- rust-mmgraph/main.rs
+++ rust-mmgraph/main.rs
@@ -1,33 +1,80 @@
 #![feature(box_syntax)]
 
+use std::io;
+use std::io::prelude::*;
+use std::fs::File;
+
 #[path = "lib.rs"]
 mod mmgraph;
-
 use mmgraph::Graph;
 
 
+fn run_assignment() -> io::Result<()> {
+    let mut s = String::new();
+    let mut f = try!(File::open("edges.txt"));
+    try!(f.read_to_string(&mut s));
+
+    let header: Vec<usize> = s.splitn(3, char::is_whitespace)
+                              .take(2)
+                              .map(|n| n.parse().unwrap())
+                              .collect();
+    let number_of_nodes = header[0];
+    let _numer_of_edges = header[1];
+
+    let mut g = Graph::with_capacity(number_of_nodes);
+
+    s.lines()
+     .skip(1)
+     .map(|line| {
+         let vals: Vec<i64> = line.trim()
+                                  .split(' ')
+                                  .map(|s| s.parse::<i64>().unwrap())
+                                  .collect();
+         let u = vals[0];
+         let v = g.find_or_create_node(vals[1]);
+         let w = vals[2];
+
+         g.with_found_or_new_node(u).create_rel_to(v, w);
+     })
+     .last();
+
+    println!("\n{}",
+             g.to_dot());
+    Ok(())
+
+}
 
 fn main() {
-    let mut g = Graph::new();
-    let mut v = g.create_node();
-    let mut x = g.create_node_with_name(666);
-    let mut u = g.create_node_with_name(999);
-    let w = g.create_node_with_name(999);
-    let q = g.create_node_with_name(555);
+    let mut g = Graph::<_, i32>::new();
+    let v = g.create_node("A");
+    let x = g.create_node("B");
+    let u = g.create_node("C");
+    let w = g.create_node("D");
+    let q = g.create_node("E");
 
-    assert!(!q.has_rel());
+    // println!("\n{}", g.to_dot());
+    g.node_mut(v).as_mut().map(|v| v.create_rel_to(x, 233));
+    g.with_new_node("F").create_rel_to(v, 45);
+    g.node_mut(v).as_mut().map(|v| v.create_rel_to(u, 233));
+    g.node_mut(q).as_mut().map(|q| q.create_rel_to(v, 233));
 
-    v.create_rel_to(&q);
-    assert!(q.has_rel());
+    // println!("\n{}", g.to_dot());
 
-    v.create_rel_to(&w);
-    v.create_rel_to(&x);
-    x.create_rel_to(&v);
-    u.create_rel_to(&x);
-    println!("g: {:?}", g);
 
-    println!("V => {:?}", *v);
-    assert!(v.has_rel());
-
-    println!("\n{}", g.to_dot());
+    run_assignment();
+    // assert!(!q.has_rel());
+    //
+    // v.create_rel_to(&q);
+    // assert!(q.has_rel());
+    //
+    // v.create_rel_to(&w);
+    // v.create_rel_to(&x);
+    // x.create_rel_to(&v);
+    // u.create_rel_to(&x);
+    // println!("g: {:?}", g);
+    //
+    // println!("V => {:?}", *v);
+    // assert!(v.has_rel());
+    //
+    //
 }
