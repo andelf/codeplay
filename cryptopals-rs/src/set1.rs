2cfb0d85898d22fcea45a84e00a9f2302df3e9d0
andelf <andelf@gmail.com>
2016-10-13 22:07:42 +0800

    set1: Implement repeating-key XOR

--- cryptopals-rs/src/set1.rs
+++ cryptopals-rs/src/set1.rs
@@ -4,6 +4,28 @@ extern crate itertools;
 use self::itertools::Itertools;
 
 
+pub fn hex_to_bytes(encoded: &[u8]) -> Vec<u8> {
+    assert_eq!(encoded.len() & 2, 0);
+    let mut bytes = Vec::with_capacity(encoded.len() / 2);
+    for hex in encoded.chunks(2) {
+        let byte = unsafe { u8::from_str_radix(str::from_utf8_unchecked(hex), 16).unwrap() };
+        bytes.push(byte);
+    }
+    bytes
+}
+
+
+pub fn bytes_to_hex(raw: &[u8]) -> Vec<u8> {
+    static CODES: &'static [u8] = b"0123456789abcdef";
+
+    let mut encoded = Vec::with_capacity(raw.len() * 2);
+    for &byte in raw {
+        encoded.push(CODES[(byte >> 4) as usize]);
+        encoded.push(CODES[(byte & 0b1111) as usize]);
+    }
+    encoded
+}
+
 /// Convert hex to base64
 pub fn hex_to_base64(raw: &[u8]) -> Vec<u8> {
     static CODES: &'static [u8] =
@@ -39,13 +61,6 @@ pub fn hex_to_base64(raw: &[u8]) -> Vec<u8> {
     encoded
 }
 
-#[test]
-fn test_convert_hex_to_base64() {
-    let hex_string = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let base64_string = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
-    assert_eq!(&hex_to_base64(hex_string)[..], base64_string.as_ref());
-}
-
 /// takes two equal-length buffers and produces their XOR combination
 pub fn fixed_xor(a: &[u8], b: &[u8]) -> Vec<u8> {
     assert_eq!(a.len(), b.len(), "two equal-length buffers");
@@ -62,6 +77,43 @@ pub fn fixed_xor(a: &[u8], b: &[u8]) -> Vec<u8> {
 }
 
 
+
+pub fn single_byte_xor_cipher(encoded: &[u8], cipher: u8) -> Vec<u8> {
+    let mut bytes = hex_to_bytes(encoded);
+    bytes.iter_mut().foreach(|b| *b ^= cipher);
+    bytes
+}
+
+
+pub fn repeating_key_xor(text: &[u8], key: &[u8]) -> Vec<u8> {
+    let mut ret = Vec::with_capacity(text.len());
+    for (&byte, &cipher) in text.iter().zip(key.iter().cycle()) {
+        ret.push(byte ^ cipher);
+    }
+    ret
+}
+
+
+
+#[test]
+fn test_repeating_key_xor() {
+    let text = b"Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal";
+    let should_be_encrypted = b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+
+    let encrypted = repeating_key_xor(text.as_ref(), b"ICE".as_ref());
+    assert_eq!(bytes_to_hex(&encrypted), should_be_encrypted.as_ref());
+}
+
+
+
+#[test]
+fn test_convert_hex_to_base64() {
+    let hex_string = b"49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let base64_string = b"SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t";
+    assert_eq!(&hex_to_base64(hex_string)[..], base64_string.as_ref());
+}
+
 #[test]
 fn test_fixed_xor() {
     let a = b"1c0111001f010100061a024b53535009181c";
@@ -69,24 +121,6 @@ fn test_fixed_xor() {
     let xord = b"746865206b696420646f6e277420706c6179";
 
     assert_eq!(&fixed_xor(a, b)[..], xord.as_ref());
-}
-
-
-pub fn hex_to_bytes(encoded: &[u8]) -> Vec<u8> {
-    assert_eq!(encoded.len() & 2, 0);
-    let mut bytes = Vec::with_capacity(encoded.len() / 2);
-    for hex in encoded.chunks(2) {
-        let byte = unsafe { u8::from_str_radix(str::from_utf8_unchecked(hex), 16).unwrap() };
-        bytes.push(byte);
-    }
-    bytes
-}
-
-
-pub fn single_byte_xor_cipher(encoded: &[u8], cipher: u8) -> Vec<u8> {
-    let mut bytes = hex_to_bytes(encoded);
-    bytes.iter_mut().foreach(|b| *b ^= cipher);
-    bytes
 }
 
 
