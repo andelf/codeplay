79cc36b36f19bd7d0017215bd401ab5d562502d8
andelf <andelf@gmail.com>
2016-10-19 20:22:29 +0800

    add leetcode 10

--- leetcode/src/lc10.cpp
+++ leetcode/src/lc10.cpp
@@ -8,21 +8,48 @@ using namespace std;
 class Solution {
 public:
     bool isMatch(string s, string p) {
+        auto lenS = s.size();
+        auto lenP = p.size();
 
-
-
-      return false;
+        if (lenS == 0 && lenP == 0) {
+            return true;
+        } else if (lenP == 0) {
+            return false;
+        }
+        if (lenP == 1 || p[1] != '*' ) {
+            if (lenS > 0 && (p[0] == '.' || s[0] == p[0]))
+                return isMatch(s.substr(1), p.substr(1));
+            return false;
+        } else {
+            if (lenS == 0) return isMatch(s, p.substr(2));
+            if (isMatch(s, p.substr(2))) return true;
+            for (auto i=0; i< lenS; ++i) {
+                // // `.*` matches any duplicated
+                // if ((s[i] == s[0]) && (s[0] == p[0] || p[0] == '.')) {
+                if ((s[i] == s[0] && s[0] == p[0]) || p[0] == '.') {
+                    if (isMatch(s.substr(i+1), p.substr(2))) {
+                        return true;
+                    }
+                } else {
+                    return false;
+                }
+            }
+        }
+        return false;
     }
-
 };
 
 
 int main(int argc, char *argv[])
 {
-  auto s = Solution{};
+    auto s = Solution{};
 
-  cout << s.isMatch("aa", "a") << '\n';
-  cout << s.isMatch("aa", "aa") << endl;
-  cout << s.isMatch("aa", "a*") << endl;
-  return 0;
+    cout << s.isMatch("aa", "a") << '\n';
+    cout << s.isMatch("aa", "aa") << endl;
+    cout << s.isMatch("aa", "a*") << endl;
+    cout << s.isMatch("a", ".*..a*") << endl;
+    cout << s.isMatch("", ".") << endl;
+    cout << s.isMatch("aa", ".*") << endl;
+    cout << s.isMatch("ab", ".*") << endl;
+    return 0;
 }
